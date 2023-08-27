0de895c8fe1175719000a5e70cc76eebf22b7a68
mattiasherrfurth <mattiasherrfurth@gmail.com>
2023-08-27 11:55:27 -0400

    updating command for search files to add wildcard

--- bash.sh
+++ bash.sh
@@ -35,8 +35,8 @@ exit
 # recursively search folders in directory for text string
 sudo grep -rnwl '/path/to/folder' -e 'text_to_search_for'
 
-# recursively search files in directory	by name 
-sudo find /path/to/folder -name	"filename"
+# recursively search files in directory	by name with wildcard 
+sudo find /path/to/folder -name	"*filename*"
 
 # List dependencies filtered to string
 ldd /path/to/executable | grep string-to-search
