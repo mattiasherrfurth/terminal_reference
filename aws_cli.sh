97900d8219c51feeca680e48f87f745b3a9bdcc0
Herrfurth <J20032@NGC.AUTHORITY>
2023-09-08 16:37:12 -0400

    generalized commands

--- aws_cli.sh
+++ aws_cli.sh
@@ -2,13 +2,16 @@
 aws s3 ls s3://bp-test-1 --recursive
 
 ### count objects in bucket (NOTE: needs bash) ###
-aws s3 ls s3://ng-ecsg-us-dmz-us-gov-west-1-bronxprod-gf-input/Test/ --recursive --summarize | grep "Total Objects:"
+aws s3 ls s3://bucketname/foldername/ --recursive --summarize | grep "Total Objects:"
 
 ### copy local file to an S3 URI ###
-aws s3 cp "C:\Users\j20032\OneDrive - NGC\Documents\BRONX\GF Data\WaferTrackingWIP\Bronx WIP table_6_8_2023.csv" s3://ng-ecsg-us-dmz-us-gov-west-1-bronxsandbox-gf-input/WaferTrackingWIP/
+aws s3 cp "C:\path\to\file.csv" s3://bucketname/foldername/
 
 ### copy contents of an S3 folder to a local folder ###
-aws s3 cp s3://ng-ecsg-us-dmz-us-gov-west-1-bronxprod-2/WeeklyOperationsReview "C:\Users\j20032\OneDrive - NGC\Documents\BRONX\Tek S3 Data\WeeklyOperationsReview" --recursive
+aws s3 cp s3://bucketname/foldername "C:\path\to\folder" --recursive
+
+### copy files w/ filename meeting condition to a local folder ###
+aws s3 cp s3://bucketname/foldername "C:\path\to\folder" --exclude "*" --include "*.csv" --recursive
 
 ### list lambda functions with names that contain string ###
 aws lambda list-functions --region us-gov-west-1 --query "Functions[?contains(FunctionName, `PowerDev`) == `true`].FunctionName"
