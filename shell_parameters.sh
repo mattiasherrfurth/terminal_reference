0d7915dee8f5f70cd33d886e7b426455990d5856
mattiasherrfurth <mattias@chell.myfiosgateway.com>
2024-05-28 21:22:58 -0400

    initial commit for shell_parameters

---
+++ shell_parameters.sh
@@ -0,0 +1,21 @@
+### SIMPLE MATCHING
+* # matching any number of arbitrary characters
+? # matching a single character
+
+### REGEX
+.* # any number of characters
+.+ # any one or more characters
+.  # one arbitrary character
+
+# ls - long listing
+ls -l
+# ls - filetypes
+ls -F
+
+# rm - recursive (dangerous)
+rm -r
+
+# grep - case-insensitive
+grep -i
+# grep - invert search
+grep -E
